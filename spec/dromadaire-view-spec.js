9b9c43ccaaa0d64898890b69aa7e91f543c5c157
Anthelme Bernard-Brunel <anthelme.bernard-brunel@onera.fr>
2018-02-08 14:33:27 +0100

    first commit

---
+++ spec/dromadaire-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import DromadaireView from '../lib/dromadaire-view';
+
+describe('DromadaireView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
